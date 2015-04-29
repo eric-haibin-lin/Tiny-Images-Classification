c24486b0e672d67b0162203a27cad23e92e654d3
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-04-29 17:19:49 +0800

    + more results for tuning PCA.

--- svm/run_prediction.m
+++ svm/run_prediction.m
@@ -22,106 +22,77 @@
 clear all;
 load a4data;
 
-% Training data: 200 data points from each of the 6 groups (total: 1200)
-XTrain1 = data_train(1:200,:);
-XTrain2 = data_train(501:700,:);
-XTrain3 = data_train(1001:1200,:);
-XTrain4 = data_train(1501:1700,:);
-XTrain5 = data_train(2001:2200,:);
-XTrain6 = data_train(2501:2700,:);
+% Training data: 300 data points from each of the 6 groups (total: 1800)
+XTrain1 = data_train(1:300,:);
+XTrain2 = data_train(501:800,:);
+XTrain3 = data_train(1001:1300,:);
+XTrain4 = data_train(1501:1800,:);
+XTrain5 = data_train(2001:2300,:);
+XTrain6 = data_train(2501:2800,:);
 
 XTrain = double([XTrain1 ; XTrain2 ; XTrain3 ; XTrain4 ; XTrain5 ; XTrain6]);
 
-TTrain1 = labels_train(1:200,:);
-TTrain2 = labels_train(501:700,:);
-TTrain3 = labels_train(1001:1200,:);
-TTrain4 = labels_train(1501:1700,:);
-TTrain5 = labels_train(2001:2200,:);
-TTrain6 = labels_train(2501:2700,:);
+TTrain1 = labels_train(1:300,:);
+TTrain2 = labels_train(501:800,:);
+TTrain3 = labels_train(1001:1300,:);
+TTrain4 = labels_train(1501:1800,:);
+TTrain5 = labels_train(2001:2300,:);
+TTrain6 = labels_train(2501:2800,:);
 
 TTrain = double([TTrain1 ; TTrain2 ; TTrain3 ; TTrain4 ; TTrain5 ; TTrain6]);
 
-% Validation data: 150 data points from each of the 6 groups (total: 900)
-XValid1 = data_train(201:350,:);
-XValid2 = data_train(701:850,:);
-XValid3 = data_train(1201:1350,:);
-XValid4 = data_train(1701:1850,:);
-XValid5 = data_train(2201:2350,:);
-XValid6 = data_train(2701:2850,:);
-
-XValid = double([XValid1 ; XValid2 ; XValid3 ; XValid4 ; XValid5 ; XValid6]);
-
-TValid1 = labels_train(201:350,:);
-TValid2 = labels_train(701:850,:);
-TValid3 = labels_train(1201:1350,:);
-TValid4 = labels_train(1701:1850,:);
-TValid5 = labels_train(2201:2350,:);
-TValid6 = labels_train(2701:2850,:);
-
-TValid = double([TValid1 ; TValid2 ; TValid3 ; TValid4 ; TValid5 ; TValid6]);
-
-% Test data: 150 data points from each of the 6 groups (total: 900)
-XTest1 = data_train(351:500,:);
-XTest2 = data_train(851:1000,:);
-XTest3 = data_train(1351:1500,:);
-XTest4 = data_train(1851:2000,:);
-XTest5 = data_train(2351:2500,:);
-XTest6 = data_train(2851:3000,:);
+% Test data: 200 data points from each of the 6 groups (total: 1200)
+XTest1 = data_train(301:500,:);
+XTest2 = data_train(801:1000,:);
+XTest3 = data_train(1301:1500,:);
+XTest4 = data_train(1801:2000,:);
+XTest5 = data_train(2301:2500,:);
+XTest6 = data_train(2801:3000,:);
 
 XTest = double([XTest1 ; XTest2 ; XTest3 ; XTest4 ; XTest5 ; XTest6]);
 
-TTest1 = labels_train(351:500,:);
-TTest2 = labels_train(851:1000,:);
-TTest3 = labels_train(1351:1500,:);
-TTest4 = labels_train(1851:2000,:);
-TTest5 = labels_train(2351:2500,:);
-TTest6 = labels_train(2851:3000,:);
+TTest1 = labels_train(301:500,:);
+TTest2 = labels_train(801:1000,:);
+TTest3 = labels_train(1301:1500,:);
+TTest4 = labels_train(1801:2000,:);
+TTest5 = labels_train(2301:2500,:);
+TTest6 = labels_train(2801:3000,:);
 
 TTest = double([TTest1 ; TTest2 ; TTest3 ; TTest4 ; TTest5 ; TTest6]);
 
-%combines validation set and test set.
-XCombo = [XValid; XTest];
-TCombo = [TValid; TTest];
 
 
-coef0 = [0, 100, 500, 1000, 2000, 5000, 10000];
-%gammas = [0.0001, 0.001, 0.01, 0.1, 1];
+%train with all data available, for submission
+%model = svmtrain(double(labels_train), double(data_train), '-t 1');
+%[predict_label] = svmpredict(double(zeros(1200, 1)), double(data_test), model); 
 
-sizes = size(coef0, 2);
 
-%train with all data available
-model = svmtrain(double(labels_train), double(data_train), '-t 1');
-[predict_label] = svmpredict(double(zeros(1200, 1)), double(data_test), model); 
 
-write_kaggle_csv('prediction', predict_label);
+%to tune SVM parameters
+coef0 = [0.001, 0.01, 0.1, 0, 1];
+gammas = [0.0001, 0.001, 0.01, 0.1, 1];
+paramToTune = gammas;
+sizes = size(paramToTune, 2);
 
-%{
 accuracyTrainArr = zeros(1,sizes);
-accuracyValidArr = zeros(1,sizes);
 accuracyTestArr = zeros(1,sizes);
-accuracyComboArr = zeros(1,sizes);
 
 for index = 1:sizes
     
-    model = svmtrain(TTrain, XTrain, horzcat(['-t 1 -d 3 -r', ' ', int2str(coef0(index))]));
+    model = svmtrain(TTrain, XTrain, horzcat(['-t 1 -d 3 -g', ' ', int2str(paramToTune(index))]));
     [predict_labelTrain, accuracyTrain, dec_valuesTrain] = svmpredict(TTrain, XTrain, model); 
-    [predict_labelValid, accuracyValid, dec_valuesValid] = svmpredict(TValid, XValid, model); 
     [predict_labelTest, accuracyTest, dec_valuesTest] = svmpredict(TTest, XTest, model); 
-    [predict_labelCombo, accuracyCombo, dec_valuesCombo] = svmpredict(TCombo, XCombo, model); 
-
+ 
     accuracyTrainArr(index) = accuracyTrain(1);
-    accuracyValidArr(index) = accuracyValid(1);
     accuracyTestArr(index) = accuracyTest(1);
-    accuracyComboArr(index) = accuracyCombo(1);
     
 end 
 
 
-plot(coef0, accuracyTrainArr, coef0, accuracyValidArr, coef0, accuracyTestArr, coef0, accuracyComboArr); 
-title('poly kernal degree 3 default gamma, coef 0 ~ 10000, all default settings, no PCA');
-legend('train', 'valid', 'test', 'combo');
-%ylim([40 47]);
-%}
+plot(paramToTune, accuracyTrainArr, paramToTune, accuracyTestArr); 
+title('poly kernal degree 3 default coef, gamma 0.0001 ~ 1, all default settings, no PCA');
+legend('train', 'test');
+ylim([40 47]);
 
 
 
